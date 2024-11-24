0761ec8c4a9e3a0b24c20fe80e838ecae93b6028
yassine4real <yssnabbou2002@gmail.com>
2024-11-24 17:29:22 +0100

    envoyer requette http avec Get vers le path " products\

--- src/main/java/ma/eniad/university/web/ProductRestService.java
+++ src/main/java/ma/eniad/university/web/ProductRestService.java
@@ -1,7 +1,22 @@
 package ma.eniad.university.web;
 
+import ma.eniad.university.entities.Product;
+import ma.eniad.university.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * @author $ {USER}
  **/
+@RestController
 public class ProductRestService {
+    @Autowired  //pour l'injection des dependances
+    private ProductRepository productRepository;
+    @GetMapping("/products")  //localhost:8085/products
+    public List<Product> products() {
+        return productRepository.findAll();
+    }
 }
