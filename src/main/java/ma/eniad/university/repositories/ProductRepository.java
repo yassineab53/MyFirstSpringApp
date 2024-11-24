a234dbec92ad88e0266c4474630fa11629493ab6
yassine4real <yssnabbou2002@gmail.com>
2024-11-24 14:06:02 +0100

    add method findByNameContains in the ProductRepository interface

--- src/main/java/ma/eniad/university/repositories/ProductRepository.java
+++ src/main/java/ma/eniad/university/repositories/ProductRepository.java
@@ -3,8 +3,12 @@ package ma.eniad.university.repositories;
 import ma.eniad.university.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author $ {USER}
  **/
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // methode qui permet de chercher les produits dont le nom contient mc
+    List<Product> findByNameContaining(String mc);
 }
